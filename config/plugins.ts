c9f674e96e3689e2136359ae0dd42d29251488be
sempitrnal <reynaldsampelo@gmail.com>
2025-06-13 12:00:02 +0800

    Rebuild admin panel after Cloudinary integration

--- config/plugins.ts
+++ config/plugins.ts
@@ -7,6 +7,10 @@ export default ({ env }) => ({
         api_key: env("CLOUDINARY_KEY"),
         api_secret: env("CLOUDINARY_SECRET"),
       },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
     },
   },
 });
